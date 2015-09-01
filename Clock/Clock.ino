8f3ab8d813f8658345553feb24f33de80a291116
Andre Vellori <andre@Andres-MacBook-Pro.local>
2015-09-02 00:13:47 +0100

    Added basic buttons

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -1,36 +1,87 @@
 #include <SPI.h>
-#include <Adafruit_GFX.h> 
+#include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 #include <TouchScreen.h>
 
-#include <Time.h>  
+#include <Time.h>
 
-#define YP A2  
-#define XM A1  
-#define YM 14  
-#define XP 17  
+#define YP A2
+#define XM A1
+#define YM 14
+#define XP 17
 TouchScreen ts = TouchScreen(XP, YP, XM, YM);
+
 #define TFT_CS 5
 #define TFT_DC 6
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
 #define DEBUG 1
 
+static uint8_t HSTARTX, HSTARTY, FONTSIZE, DELTAHHMM;
+static uint16_t FORECOLOR, BGCOLOR;
+static unsigned int TS_MINX, TS_MAXX, TS_MINY, TS_MAXY;
+
+Adafruit_GFX_Button buttons[4];
 void setup() {
+  HSTARTX = 10;
+  HSTARTY = 10;
+  FONTSIZE = 5;
+  DELTAHHMM = FONTSIZE * 6 / 2;
+  FORECOLOR = ILI9341_WHITE;
+  BGCOLOR = ILI9341_BLACK;
+  
+  TS_MINX = 116*2;
+  TS_MAXX = 890*2;
+  TS_MINY = 83*2;
+  TS_MAXY = 912*2;
   Serial.begin(9600);
   Serial.print("Init.");
   tft.begin();
   tft.fillScreen(ILI9341_BLACK);
   tft.setRotation(3);
-  setTime(10,10,5,1,9,2015);
+  setTime(10, 10, 5, 1, 9, 2015);
+
+  buttons[0].initButton(&tft, FONTSIZE * 8 + HSTARTX,
+                        FONTSIZE * 6 * 2+ HSTARTY, 32, 32, ILI9341_WHITE, ILI9341_ORANGE, ILI9341_WHITE, "+", 2);
+  buttons[1].initButton(&tft, FONTSIZE * 8 + HSTARTX,
+                        FONTSIZE * 6 * 3+ HSTARTY, 
+                        32, 32, ILI9341_WHITE, ILI9341_ORANGE, ILI9341_WHITE, "-", 2);
+  buttons[0].drawButton(false);
+  buttons[1].drawButton(false);
 }
 
 void loop() {
+  Point p;
+
+  if (ts.isTouching()) {
+    p = ts.getPoint();
+    
+  } else {
+    p.x = p.y = p.z = -1;
+  }
+  if (p.z != -1) {
+    p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
+    p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
+    Serial.println(String(p.y) + "  " + String(p.x));
+  }
 #if DEBUG==1
   uint32_t myTime = millis();
   boolean updated =
 #endif
-  updateTimeIfNeeded();
+    updateTimeIfNeeded();
+
+  for (uint8_t b = 0; b < 2; b++) {
+    if (buttons[b].contains(p.x, p.y)) {
+      Serial.print("Pressing: "); Serial.println(b);
+      if (!buttons[b].isPressed()) buttons[b].drawButton(true); 
+      buttons[b].press(true);  
+    } else {
+      if (buttons[b].isPressed() ) buttons[b].drawButton(false);
+      buttons[b].press(false); 
+      
+    }
+    
+  }
 #if DEBUG==1
   if (updated) {
     Serial.print(millis() - myTime, DEC);
@@ -48,7 +99,7 @@ boolean updateTimeIfNeeded()
 {
   uint8_t currentHour = hour();
   uint8_t currentMinute = minute();
-  boolean updated=false;
+  boolean updated = false;
   if (currentHour != lastHour) {
     drawHours(currentHours());
     lastHour = currentHour;
@@ -60,8 +111,8 @@ boolean updateTimeIfNeeded()
     updated = true;
   }
   boolean newIntermezz;
-  
-  newIntermezz = (second() %2) == 1;
+
+  newIntermezz = (second() % 2) == 1;
 
   if (newIntermezz != intermezz) {
     drawIntermezz(newIntermezz);
@@ -70,16 +121,10 @@ boolean updateTimeIfNeeded()
   }
   return updated;
 }
-#define HSTARTX 10
-#define HSTARTY 10
-#define FONTSIZE 5
-#define DELTAHHMM FONTSIZE*6/2 
-#define FORECOLOR ILI9341_WHITE
-#define BGCOLOR ILI9341_BLACK
 
 void drawIntermezz(boolean there)
 {
-  tft.setCursor(HSTARTX + (FONTSIZE * 6  * 2) - (DELTAHHMM/2) -1,HSTARTY);
+  tft.setCursor(HSTARTX + (FONTSIZE * 6  * 2) - (DELTAHHMM / 2) - 1, HSTARTY);
   tft.setTextSize(FONTSIZE);
   if (there)  {
     tft.setTextColor(FORECOLOR);
@@ -89,31 +134,31 @@ void drawIntermezz(boolean there)
   tft.println(":");
 }
 
-void drawHours(String hours) 
+void drawHours(String hours)
 {
-  tft.setCursor(HSTARTX,HSTARTY);
+  tft.setCursor(HSTARTX, HSTARTY);
   tft.setTextSize(FONTSIZE);
-  tft.setTextColor(FORECOLOR,BGCOLOR);
+  tft.setTextColor(FORECOLOR, BGCOLOR);
   tft.println(hours);
 }
 
-void drawMinutes(String minutes) 
+void drawMinutes(String minutes)
 {
-  tft.setCursor(HSTARTX + (FONTSIZE * 6 * 2) + DELTAHHMM,HSTARTY);
+  tft.setCursor(HSTARTX + (FONTSIZE * 6 * 2) + DELTAHHMM, HSTARTY);
   tft.setTextSize(FONTSIZE);
-  tft.setTextColor(FORECOLOR,BGCOLOR);
+  tft.setTextColor(FORECOLOR, BGCOLOR);
   tft.println(minutes);
 }
 
-String currentHours() 
+String currentHours()
 {
   String hourS = String(hour());
-  return hourS.length()==1? " " + hourS:hourS;
+  return hourS.length() == 1 ? " " + hourS : hourS;
 }
 
-String currentMinutes() 
+String currentMinutes()
 {
   String minuteS = String(minute());
-  return minuteS.length()==1? "0" + minuteS:minuteS;
+  return minuteS.length() == 1 ? "0" + minuteS : minuteS;
 }
 
