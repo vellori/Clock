17466522273889d8ac6773bb3e49bdd56173fa32
André <andre@vellori.it>
2015-09-04 07:00:04 +0100

    Added: Tap to show commands and they hide after 5 seconds

--- Clock/Clock.ino
+++ Clock/Clock.ino
@@ -7,114 +7,145 @@
 
 #define YP A2
 #define XM A1
-#define YM 18
-#define XP 21
+#define YM 14
+#define XP 17
 TouchScreen ts = TouchScreen(XP, YP, XM, YM);
+#define DEBUG 1
+#define debug(x) if (DEBUG) { Serial.println(x); }
 
 #define TFT_CS 5
 #define TFT_DC 6
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 
-#define DEBUG 0
-
- #define HSTARTX 15
- #define HSTARTY  90
- #define FONTSIZE 7
- #define LETTERWIDTH FONTSIZE*8
- #define LETTERHEIGHT FONTSIZE*6
- #define DELTAHHMM FONTSIZE * 6 / 2
- #define FORECOLOR ILI9341_WHITE
- #define BGCOLOR ILI9341_BLACK
- #define BUTTONBG ILI9341_ORANGE
- #define BUTTONTEXT ILI9341_WHITE
- #define BUTTONLINE ILI9341_WHITE
- #define TS_MINX 116*2
- #define TS_MAXX 890*2
- #define TS_MINY 83*2
- #define TS_MAXY 912*2
-
+#define HSTARTX 15
+#define HSTARTY  90
+#define FONTSIZE 7
+#define LETTERWIDTH FONTSIZE*8
+#define LETTERHEIGHT FONTSIZE*6
+#define DELTAHHMM FONTSIZE * 6 / 2
+#define FORECOLOR ILI9341_WHITE
+#define BGCOLOR ILI9341_BLACK
+#define BUTTONBG ILI9341_ORANGE
+#define BUTTONTEXT ILI9341_WHITE
+#define BUTTONLINE ILI9341_WHITE
+#define TS_MINX 116*2
+#define TS_MAXX 890*2
+#define TS_MINY 83*2
+#define TS_MAXY 912*2
+#define NUMBUTTONS 4
 Adafruit_GFX_Button buttons[4];
-char plus[]="+";
-char minus[]="-";
+char plus[] = "+";
+char minus[] = "-";
+
+boolean buttonShown;
+
 void setup() {
 
   Serial.begin(9600);
-  Serial.println("Init");
+  debug("init");
   tft.begin();
-  tft.fillScreen(BGCOLOR);
+  initScreen();
   tft.setRotation(3);
-  if (timeStatus()==timeNotSet) {
-    Serial.println("Clock");
+  if (timeStatus() == timeNotSet) {
+    debug("clock");
     setTime(10, 10, 5, 1, 9, 2015);
   }
 
-  buttons[0].initButton(&tft, 
-                        HSTARTX+LETTERWIDTH/2,
-                        HSTARTY - LETTERHEIGHT , 
-                        LETTERWIDTH *1.5, 
-                        LETTERHEIGHT*1.5, 
-                        BUTTONLINE, 
-                        BUTTONBG, 
-                        BUTTONLINE, 
+  buttons[0].initButton(&tft,
+                        HSTARTX + LETTERWIDTH / 2,
+                        HSTARTY - LETTERHEIGHT ,
+                        LETTERWIDTH * 1.5,
+                        LETTERHEIGHT * 1.5,
+                        BUTTONLINE,
+                        BUTTONBG,
+                        BUTTONLINE,
                         plus, 2);
-  buttons[1].initButton(&tft, 
-                        HSTARTX+LETTERWIDTH/2,
-                        LETTERHEIGHT * 2+ HSTARTY+2, 
-                        LETTERWIDTH*1.5, 
-                        LETTERHEIGHT*1.5, 
-                        BUTTONLINE, 
-                        BUTTONBG, 
-                        BUTTONLINE, 
+  buttons[1].initButton(&tft,
+                        HSTARTX + LETTERWIDTH / 2,
+                        LETTERHEIGHT * 2 + HSTARTY + 2,
+                        LETTERWIDTH * 1.5,
+                        LETTERHEIGHT * 1.5,
+                        BUTTONLINE,
+                        BUTTONBG,
+                        BUTTONLINE,
                         minus, 2);
-buttons[2].initButton(&tft, 
-                        HSTARTX+LETTERWIDTH*2 + DELTAHHMM + LETTERWIDTH/2,
-                        HSTARTY - LETTERHEIGHT , 
-                        LETTERWIDTH *1.5, 
-                        LETTERHEIGHT*1.5, 
-                        BUTTONLINE, 
-                        BUTTONBG, 
-                        BUTTONLINE, 
+  buttons[2].initButton(&tft,
+                        HSTARTX + LETTERWIDTH * 2 + DELTAHHMM + LETTERWIDTH / 2,
+                        HSTARTY - LETTERHEIGHT ,
+                        LETTERWIDTH * 1.5,
+                        LETTERHEIGHT * 1.5,
+                        BUTTONLINE,
+                        BUTTONBG,
+                        BUTTONLINE,
                         plus, 2);
-  buttons[3].initButton(&tft, 
-                        HSTARTX+LETTERWIDTH*2 + DELTAHHMM + LETTERWIDTH/2,
-                        LETTERHEIGHT * 2+ HSTARTY+2, 
-                        LETTERWIDTH*1.5, 
-                        LETTERHEIGHT*1.5, 
-                        BUTTONLINE, 
-                        BUTTONBG, 
-                        BUTTONLINE, 
+  buttons[3].initButton(&tft,
+                        HSTARTX + LETTERWIDTH * 2 + DELTAHHMM + LETTERWIDTH / 2,
+                        LETTERHEIGHT * 2 + HSTARTY + 2,
+                        LETTERWIDTH * 1.5,
+                        LETTERHEIGHT * 1.5,
+                        BUTTONLINE,
+                        BUTTONBG,
+                        BUTTONLINE,
                         minus, 2);
-  buttons[0].drawButton(false);
-  buttons[1].drawButton(false);
-  buttons[2].drawButton(false);
-  buttons[3].drawButton(false);
+  drawButtons(buttonShown);
 }
 
+void drawButtons(boolean showThem) {
+  if (showThem) {
+    for (int i; i < NUMBUTTONS; i++) {
+      buttons[i].drawButton(false);
+    }
+  } else {
+    initScreen();
+  }
+}
+
+void initScreen() {
+  tft.fillScreen(BGCOLOR);
+  debug("IS");
+}
+uint32_t lastTouch;
 void loop() {
   Point p;
-
-  if (ts.isTouching()) {
-    p = ts.getPoint();
+  boolean refreshScreen;
+  boolean forceUpdate=false;
+  p = ts.getPoint();
+  if (ts.isTouching() & (p.z > 300)) {
+    if (!buttonShown) {
+      buttonShown = true;
+      drawButtons(buttonShown);
+    }
+    lastTouch = millis();
   } else {
     p.x = p.y = p.z = -1;
+    if ((millis() - lastTouch) > 5000) {
+      if (buttonShown) {
+        buttonShown = false;
+        drawButtons(buttonShown);
+        forceUpdate = true;
+      }
+    }
   }
+
   if (p.z != -1) {
+    Serial.println("X: " + String(p.x) + " Y:" + String(p.y));
     uint16_t temp  = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
-    p.x =tft.height()- map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
+    p.x = tft.height() - map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
     p.y = temp;
+    
   }
 #if DEBUG==1
   uint32_t myTime = millis();
   boolean updated =
 #endif
-    updateTimeIfNeeded();
-    manageButtons(p);
-    reactButtons();
+    updateTimeIfNeeded(forceUpdate);
+  manageButtons(p);
+  reactButtons();
 
 #if DEBUG==1
   if (updated) {
     Serial.print(millis() - myTime, DEC);
-    Serial.println(" ms");
+    debug("ms");
   }
 #endif
 }
@@ -124,17 +155,17 @@ void loop() {
 uint8_t lastHour;
 uint8_t lastMinute;
 boolean intermezz;
-inline boolean updateTimeIfNeeded()
+boolean updateTimeIfNeeded(boolean force)
 {
   uint8_t currentHour = hour();
   uint8_t currentMinute = minute();
   boolean updated = false;
-  if (currentHour != lastHour) {
+  if ((currentHour != lastHour) | force) {
     drawHours(currentHours());
     lastHour = currentHour;
     updated = true;
   }
-  if (currentMinute != lastMinute) {
+  if ((currentMinute != lastMinute) | force) {
     drawMinutes(currentMinutes());
     lastMinute = currentMinute;
     updated = true;
@@ -151,7 +182,7 @@ inline boolean updateTimeIfNeeded()
   return updated;
 }
 
-inline void drawIntermezz(boolean there)
+void drawIntermezz(boolean there)
 {
   tft.setCursor(HSTARTX + (FONTSIZE * 6  * 2) - (DELTAHHMM / 2) - 1, HSTARTY);
   tft.setTextSize(FONTSIZE);
@@ -163,7 +194,7 @@ inline void drawIntermezz(boolean there)
   tft.println(":");
 }
 
-inline void drawHours(String hours)
+void drawHours(String hours)
 {
   tft.setCursor(HSTARTX, HSTARTY);
   tft.setTextSize(FONTSIZE);
@@ -171,7 +202,7 @@ inline void drawHours(String hours)
   tft.println(hours);
 }
 
-inline void drawMinutes(String minutes)
+void drawMinutes(String minutes)
 {
   tft.setCursor(HSTARTX + (FONTSIZE * 6 * 2) + DELTAHHMM, HSTARTY);
   tft.setTextSize(FONTSIZE);
@@ -179,34 +210,39 @@ inline void drawMinutes(String minutes)
   tft.println(minutes);
 }
 
-inline String currentHours()
+String currentHours()
 {
   String hourS = String(hour());
   return hourS.length() == 1 ? " " + hourS : hourS;
 }
 
-inline String currentMinutes()
+String currentMinutes()
 {
   String minuteS = String(minute());
   return minuteS.length() == 1 ? "0" + minuteS : minuteS;
 }
 
-inline void manageButtons(Point p)
+void manageButtons(Point p)
 {
-    for (uint8_t b = 0; b < 4; b++) {
-    if (buttons[b].contains(p.x, p.y)) {
-      if (!buttons[b].isPressed()) buttons[b].drawButton(true); 
-      buttons[b].press(true);  
-    } else {
-      if (buttons[b].isPressed() ) buttons[b].drawButton(false);
-      buttons[b].press(false); 
+  if (buttonShown)  {
+    for (uint8_t b = 0; b < NUMBUTTONS; b++) {
+      if (buttons[b].contains(p.x, p.y)) {
+        if (!buttons[b].isPressed()) buttons[b].drawButton(true);
+        buttons[b].press(true);
+      } else {
+        if (buttons[b].isPressed() ) buttons[b].drawButton(false);
+        buttons[b].press(false);
+      }
     }
   }
 }
 
-inline void reactButtons() {
- int adj[] = {60*60,-60*60,60,-60};
-  for (int i=0;i<4;i++) {
+void reactButtons() {
+  if (!buttonShown) {
+    return;
+  }
+  int adj[] = {60 * 60, -60 * 60, 60, -60};
+  for (int i = 0; i < NUMBUTTONS; i++) {
     if (buttons[i].isPressed() & buttons[i].justPressed()) {
       adjustTime(adj[i]);
     }
